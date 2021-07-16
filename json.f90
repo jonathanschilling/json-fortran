b569c99bf2e39b58e470669587bf51ce9c7ab67b
Jonathan Schilling <jonathan.schilling@ipp.mpg.de>
2021-07-16 12:34:03 +0200

    up to 5 dimensions; now need more data types

--- json.f90
+++ json.f90
@@ -52,10 +52,10 @@ subroutine add_array_1d(name, n, content)
   has_previous = .true.
 end subroutine add_array_1d
 
-subroutine add_array_2d(name, n2, n1, content)
+subroutine add_array_2d(name, n1, n2, content)
   character(len=*), intent(in) :: name
-  integer, intent(in) :: n2, n1
-  character(len=*), dimension(n2,n1), intent(in) :: content
+  integer, intent(in) :: n1, n2
+  character(len=*), dimension(n1,n2), intent(in) :: content
 
   integer :: i, j
 
@@ -64,16 +64,16 @@ subroutine add_array_2d(name, n2, n1, content)
   end if
 
   write(iunit, '(3A)', advance="no") '"',trim(adjustl(name)),'":['
-  do i = 1, n1
+  do j = 1, n2
     write(iunit, '(A)', advance="no") '['
-    do j = 1, n2
-      write(iunit, '(2A)', advance="no") trim(adjustl(content(j, i)))
-      if (j .lt. n2) then
+    do i = 1, n1
+      write(iunit, '(2A)', advance="no") trim(adjustl(content(i,j)))
+      if (i .lt. n1) then
         write(iunit, '(2A)', advance="no") ','
       end if
     end do
     write(iunit, '(2A)', advance="no") ']'
-    if (i .lt. n1) then
+    if (j .lt. n2) then
       write(iunit, '(2A)', advance="no") ','
     end if
   end do
@@ -82,10 +82,10 @@ subroutine add_array_2d(name, n2, n1, content)
   has_previous = .true.
 end subroutine add_array_2d
 
-subroutine add_array_3d(name, n3, n2, n1, content)
+subroutine add_array_3d(name, n1, n2, n3, content)
   character(len=*), intent(in) :: name
-  integer, intent(in) :: n3, n2, n1
-  character(len=*), dimension(n3,n2,n1), intent(in) :: content
+  integer, intent(in) :: n1, n2, n3
+  character(len=*), dimension(n1,n2,n3), intent(in) :: content
 
   integer :: i, j, k
 
@@ -94,13 +94,13 @@ subroutine add_array_3d(name, n3, n2, n1, content)
   end if
 
   write(iunit, '(3A)', advance="no") '"',trim(adjustl(name)),'":['
-  do i = 1, n1
+  do k = 1, n3
     write(iunit, '(A)', advance="no") '['
     do j = 1, n2
       write(iunit, '(A)', advance="no") '['
-      do k = 1, n3
-        write(iunit, '(2A)', advance="no") trim(adjustl(content(k,j,i)))
-        if (k .lt. n3) then
+      do i = 1, n1
+        write(iunit, '(2A)', advance="no") trim(adjustl(content(i,j,k)))
+        if (i .lt. n1) then
           write(iunit, '(2A)', advance="no") ','
         end if
       end do
@@ -110,7 +110,7 @@ subroutine add_array_3d(name, n3, n2, n1, content)
       end if
     end do
     write(iunit, '(2A)', advance="no") ']'
-    if (i .lt. n1) then
+    if (k .lt. n3) then
       write(iunit, '(2A)', advance="no") ','
     end if
   end do
@@ -118,6 +118,101 @@ subroutine add_array_3d(name, n3, n2, n1, content)
 
   has_previous = .true.
 end subroutine add_array_3d
+
+subroutine add_array_4d(name, n1, n2, n3, n4, content)
+  character(len=*), intent(in) :: name
+  integer, intent(in) :: n1, n2, n3, n4
+  character(len=*), dimension(n1,n2,n3,n4), intent(in) :: content
+
+  integer :: i, j, k, l
+
+  if (has_previous) then
+    write(iunit, '(A)', advance="no") ','
+  end if
+
+  write(iunit, '(3A)', advance="no") '"',trim(adjustl(name)),'":['
+  do l = 1, n4
+    write(iunit, '(A)', advance="no") '['
+    do k = 1, n3
+      write(iunit, '(A)', advance="no") '['
+      do j = 1, n2
+        write(iunit, '(A)', advance="no") '['
+        do i = 1, n1
+          write(iunit, '(2A)', advance="no") trim(adjustl(content(i,j,k,l)))
+          if (i .lt. n1) then
+            write(iunit, '(2A)', advance="no") ','
+          end if
+        end do
+        write(iunit, '(2A)', advance="no") ']'
+        if (j .lt. n2) then
+          write(iunit, '(2A)', advance="no") ','
+        end if
+      end do
+      write(iunit, '(2A)', advance="no") ']'
+      if (k .lt. n3) then
+        write(iunit, '(2A)', advance="no") ','
+      end if
+    end do
+    write(iunit, '(2A)', advance="no") ']'
+    if (l .lt. n4) then
+      write(iunit, '(2A)', advance="no") ','
+    end if
+  end do
+  write(iunit, '(2A)', advance="no") ']'
+
+  has_previous = .true.
+end subroutine add_array_4d
+
+subroutine add_array_5d(name, n1, n2, n3, n4, n5, content)
+  character(len=*), intent(in) :: name
+  integer, intent(in) :: n1, n2, n3, n4, n5
+  character(len=*), dimension(n1,n2,n3,n4,n5), intent(in) :: content
+
+  integer :: i, j, k, l, m
+
+  if (has_previous) then
+    write(iunit, '(A)', advance="no") ','
+  end if
+
+  write(iunit, '(3A)', advance="no") '"',trim(adjustl(name)),'":['
+  do m = 1, n5
+    write(iunit, '(A)', advance="no") '['
+    do l = 1, n4
+      write(iunit, '(A)', advance="no") '['
+      do k = 1, n3
+        write(iunit, '(A)', advance="no") '['
+        do j = 1, n2
+          write(iunit, '(A)', advance="no") '['
+          do i = 1, n1
+            write(iunit, '(2A)', advance="no") trim(adjustl(content(i,j,k,l,m)))
+            if (i .lt. n1) then
+              write(iunit, '(2A)', advance="no") ','
+            end if
+          end do
+          write(iunit, '(2A)', advance="no") ']'
+          if (j .lt. n2) then
+            write(iunit, '(2A)', advance="no") ','
+          end if
+        end do
+        write(iunit, '(2A)', advance="no") ']'
+        if (k .lt. n3) then
+          write(iunit, '(2A)', advance="no") ','
+        end if
+      end do
+      write(iunit, '(2A)', advance="no") ']'
+      if (l .lt. n4) then
+        write(iunit, '(2A)', advance="no") ','
+      end if
+    end do
+    write(iunit, '(2A)', advance="no") ']'
+    if (m .lt. n5) then
+      write(iunit, '(2A)', advance="no") ','
+    end if
+  end do
+  write(iunit, '(2A)', advance="no") ']'
+
+  has_previous = .true.
+end subroutine add_array_5d
 
 subroutine add_int(name, val)
   character(len=*), intent(in) :: name
@@ -146,44 +241,92 @@ subroutine add_int_1d(name, n, arr)
   deallocate(temp)
 end subroutine add_int_1d
 
-subroutine add_int_2d(name, n2, n1, arr)
+subroutine add_int_2d(name, n1, n2, arr)
   character(len=*), intent(in) :: name
-  integer, intent(in) :: n2, n1
-  integer, dimension(n2, n1), intent(in) :: arr
+  integer, intent(in) :: n1, n2
+  integer, dimension(n1, n2), intent(in) :: arr
 
   character(len=64), dimension(:,:), allocatable :: temp
   integer :: i, j
 
-  allocate(temp(n2, n1))
+  allocate(temp(n1, n2))
   do i = 1, n1
     do j = 1, n2
-      write(temp(j,i), *) arr(j,i)
+      write(temp(i,j), *) arr(i,j)
     end do
   end do
-  call add_array_2d(name, n2, n1, temp)
+  call add_array_2d(name, n1, n2, temp)
 
   deallocate(temp)
 end subroutine add_int_2d
 
-subroutine add_int_3d(name, n3, n2, n1, arr)
+subroutine add_int_3d(name, n1, n2, n3, arr)
   character(len=*), intent(in) :: name
-  integer, intent(in) :: n3, n2, n1
-  integer, dimension(n3, n2, n1), intent(in) :: arr
+  integer, intent(in) :: n1, n2, n3
+  integer, dimension(n1, n2, n3), intent(in) :: arr
 
   character(len=64), dimension(:,:,:), allocatable :: temp
   integer :: i, j, k
 
-  allocate(temp(n3,n2,n1))
+  allocate(temp(n1,n2,n3))
   do i = 1, n1
     do j = 1, n2
       do k = 1, n3
-        write(temp(k,j,i), *) arr(k,j,i)
+        write(temp(i,j,k), *) arr(i,j,k)
       end do
     end do
   end do
-  call add_array_3d(name, n3, n2, n1, temp)
+  call add_array_3d(name, n1, n2, n3, temp)
 
   deallocate(temp)
 end subroutine add_int_3d
+
+subroutine add_int_4d(name, n1, n2, n3, n4, arr)
+  character(len=*), intent(in) :: name
+  integer, intent(in) :: n1, n2, n3, n4
+  integer, dimension(n1,n2,n3,n4), intent(in) :: arr
+
+  character(len=64), dimension(:,:,:,:), allocatable :: temp
+  integer :: i, j, k, l
+
+  allocate(temp(n1,n2,n3,n4))
+  do i = 1, n1
+    do j = 1, n2
+      do k = 1, n3
+        do l = 1, n4
+          write(temp(i,j,k,l), *) arr(i,j,k,l)
+        end do
+      end do
+    end do
+  end do
+  call add_array_4d(name, n1, n2, n3, n4, temp)
+
+  deallocate(temp)
+end subroutine add_int_4d
+
+subroutine add_int_5d(name, n1, n2, n3, n4, n5, arr)
+  character(len=*), intent(in) :: name
+  integer, intent(in) :: n1, n2, n3, n4, n5
+  integer, dimension(n1,n2,n3,n4,n5), intent(in) :: arr
+
+  character(len=64), dimension(:,:,:,:,:), allocatable :: temp
+  integer :: i, j, k, l, m
+
+  allocate(temp(n1,n2,n3,n4, n5))
+  do i = 1, n1
+    do j = 1, n2
+      do k = 1, n3
+        do l = 1, n4
+          do m = 1, n5
+            write(temp(i,j,k,l,m), *) arr(i,j,k,l,m)
+          end do
+        end do
+      end do
+    end do
+  end do
+  call add_array_5d(name, n1, n2, n3, n4, n5, temp)
+
+  deallocate(temp)
+end subroutine add_int_5d
 
 end module json
